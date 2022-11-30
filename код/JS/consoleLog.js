c0e05476476df9ed9e975f0964772cd721406bc0
TonyaMazurova <118722912+TinyTonya@users.noreply.github.com>
2022-11-30 13:11:29 +0300

    5

---
+++ код/JS/consoleLog.js
@@ -0,0 +1,4 @@
+const car = 'Mercedes';
+let bus = 'Volvo';
+let carSharing = car; 
+console.log (car, bus, carSharing);
---
+++ код/JS/objectCar.js
@@ -0,0 +1,6 @@
+ let car = {
+   engine: '249 hp',
+   turbo: true,
+   maxSpeed: 260
+ };
+console.log(typeof car);
