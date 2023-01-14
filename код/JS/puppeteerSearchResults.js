e9c0fdac256f808a421eea69ec051ee291c5d489
TonyaMazurova <118722912+TinyTonya@users.noreply.github.com>
2023-01-14 13:15:29 +0300

    Create puppeteerSearchResults.js

---
+++ код/JS/puppeteerSearchResults.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+
+async function testYaRu(){
+    console.log('Запуск браузера');
+    const browser = await puppeteer.launch();
+
+    console.log('Создание новой вкладки в браузере');
+    const page = await browser.newPage();
+
+    console.log('Переход на страницу ya.ru');
+    await page.goto('https://ya.ru/');
+
+    console.log('Ввод текста "Автоматизация тестирования" в поисковую строку');
+    const searchField = await page.$('#text');
+    await searchField.type('Автоматизация тестирования');
+
+   console.log('Клик в кнопку "Найти"');
+    const searchButton = await page.$('.button[type=submit]');// создай константу searchButton и присвоей ей результат поиска элемента кнопки "Найти"
+        await searchButton.click();
+    
+    console.log('Ожидание перехода в страницу поисковых результатов');
+    await page.waitForNavigation();// напиши команду ожидания загрузки результата
+
+    console.log('Получение элементов результата поиска');
+       const result = await page.$('.serp-item'); // создай переменную result и положи в неё элемент поисковой выдачи
+
+    console.log('Сравнение ОР и ФР');
+    if (result == null) {
+        console.log("Результаты поиска не найдены");
+    }
+    else {
+       console.log("Результаты поиска отобразились");
+    }
+    
+    console.log('Закрытие браузера');
+    await browser.close();
+}
+
+testYaRu();
