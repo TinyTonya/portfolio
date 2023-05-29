c917fc1675626b640077afa705b0d44aa27733d5
Antonina <118722912+TinyTonya@users.noreply.github.com>
2023-05-29 09:21:39 +0300

    Create lesson6_step5.py

---
+++ selenium_course/lesson6_step5.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    linkn = browser.find_element(By.PARTIAL_LINK_TEXT, "224592")
+    linkn.click()
+
+    input1 = browser.find_element(By.TAG_NAME, "input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn.btn-default")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
