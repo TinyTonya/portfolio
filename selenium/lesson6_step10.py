0928ba6b200cd09a1c26c0d9e6ad21bda2fce877
Antonina <118722912+TinyTonya@users.noreply.github.com>
2023-06-03 20:49:51 +0300

    Create lesson6_step10.py

---
+++ selenium/lesson6_step10.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.first")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.second")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.third")
+    input3.send_keys("Smolensk")
+    ...
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(5)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
